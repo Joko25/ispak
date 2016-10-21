da1f44660fd21684c763e0cd912ddddcbdaf6eed
Joko Mulyono <jokomulyono696@gmail.com>
2016-10-21 13:46:04 +0700

    adding master ispak.co.id

---
+++ api/config.php
@@ -0,0 +1,7 @@
+    // Consumer Key
+    define('CONSUMER_KEY', '1ayGCgViajgshiZKx6w4riTjH');
+    define('CONSUMER_SECRET', 'pPqAKXZBZrvO3DGryv2hBjjwMQH9NGZJl0hz4KKQFqBaFwIRLH);
+
+    // User Access Token
+    define('ACCESS_TOKEN', ' 4864707734-GtqaECAxGCajOhyAQpKdqqKUHYKrWgQO9wSIHuM');
+    define('ACCESS_SECRET', 'KESrgp6fa60IvAjDiBZTwvHHIpKxexFeMIg1B2qXBqqYy');
